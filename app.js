e4f1c986dde7133951ec65e97aee9e04f05662b8
Payad <swordoflight8847@yahoo.com>
2023-10-23 15:14:42 -0700

    added functionality to buttons

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 //Pomodoro Timer function
 
 // const digits = document.querySelector('.digits');
+const title = document.querySelector('.title');
+const breakBtn = document.querySelector('.break');
+breakBtn.style.display = 'none';
+const stopBtn = document.querySelector('.stop');
+stopBtn.style.display = 'none'
 
 function Timer(duration, display) {
     var time = duration, minute, second;
@@ -18,13 +23,19 @@ function Timer(duration, display) {
         // display = document.querySelector('.digits');
         display.innerText = minute + ":" + second;
 
-        const stopBtn = document.querySelector('.stop');
+        // const stopBtn = document.querySelector('.stop');
         stopBtn.addEventListener('click', () => {
+        title.innerHTML = 'Pause';
+        breakBtn.style.display = 'none';
+        startBtn.style.display = 'inline-block';
+        stopBtn.style.display = 'none';
         clearInterval(timer)
 });
 
-        const breakBtn = document.querySelector('.break');
+        // const breakBtn = document.querySelector('.break');
+        // breakBtn.style.display = 'none';
         breakBtn.addEventListener('click', () => {
+            title.innerHTML = 'Break'
             clearInterval(timer);
 
             minute = parseInt(time / 60, 10);
@@ -43,13 +54,25 @@ function Timer(duration, display) {
 }
 
 const startBtn = document.querySelector('.start');
-
+// startBtn.style.backgroundColor = 'yellow';
 startBtn.addEventListener('click', () => {
+    title.innerHTML = 'Work';
     const twentyFive = 60 * 25;
     display = document.querySelector('.digits');
+    breakBtn.style.display = 'inline-block';
+    // breakBtn.style.marginLeft = 'auto';
+    // breakBtn.style.marginRight = 'auto';
+    stopBtn.style.display = 'inline-block';
+    // stopBtn.style.marginLeft = 'auto';
+    // stopBtn.style.marginRight = 'auto';
+    startBtn.style.display = 'none';
     Timer(twentyFive, display);
 })
 
+// const imgBtn = document.getElementsByClassName('.start')[0];
+// if (startBtn) {
+//     startBtn.style.display = 'none'
+// }
 // const stopBtn = document.querySelector('.stop');
 
 // stopBtn.addEventListener('click', () => {
